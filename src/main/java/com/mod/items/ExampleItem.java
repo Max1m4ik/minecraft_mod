1798cefa776cf0e3a772ab5d952b6efbceb74003
Sem <sem.semechko@mail.ru>
2025-03-06 20:59:11 +0700

    semen first commit

--- src/main/java/com/mod/items/ExampleItem.java
+++ src/main/java/com/mod/items/ExampleItem.java
@@ -1,5 +1,8 @@
 package com.mod.items;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Settings;
+
 public class ExampleItem extends Item {
     public ExampleItem(Settings settings) {
         super(settings);
--- src/main/java/com/mod/mod.java
+++ src/main/java/com/mod/mod.java
@@ -1,21 +1,18 @@
 package com.mod;
 
+import com.mod.items.ExampleItem;
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class mod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Item EXAMPLE_ITEM = new ExampleItem(new Item.Settings());
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ITEM, new Identifier("modid", "example_item"), EXAMPLE_ITEM);
-		LOGGER.info("Hello Fabric world!");
+		// Используйте Identifier.of вместо new Identifier
+		Registry.register(Registries.ITEM, Identifier.of("modid", "example_item"), EXAMPLE_ITEM);
 	}
 }
